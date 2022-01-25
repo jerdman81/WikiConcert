438000c824b6ee14405f89d0636d75aee704030e
Patom Thirawetchayan <patomt@gmail.com>
2022-01-25 14:18:32 -0500

    create SongUpdateMethod and associated model

---
+++ WikiConcert.Models/SongUpdate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiConcert.Models
+{
+    public class SongUpdate
+    {
+        public int SongId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Artist { get; set; }
+
+        public DateTimeOffset ReleaseDate { get; set; }
+        public string Lyrics { get; set; }
+    }
+}
