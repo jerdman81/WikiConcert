99d8aa401d9dfbc9398a0e56e19b997126ae7ce1
Patom Thirawetchayan <patomt@gmail.com>
2022-01-25 09:47:23 -0500

    created band update method

---
+++ WikiConcert.Models/BandUpdate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiConcert.Models
+{
+    public class BandUpdate
+    {
+        public int BandId { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public bool IsActive { get; set; }
+        public DateTimeOffset Modified { get; set; }
+    }
+}
