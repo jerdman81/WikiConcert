d41aa269ade5b9929afa4b2d82757f1c12048e79
Patom Thirawetchayan <patomt@gmail.com>
2022-01-31 10:59:55 -0500

    create attendance object

--- WikiConcert.Data/Attendance.cs
+++ WikiConcert.Data/Attendance.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WikiConcert.Data
 {
-    internal class Attendance
+    public class Attendance
     {
+        [Key]
+        public int AttendanceId { get; set; }
+        [Required, ForeignKey (nameof(Concert))]
+        public int ConcertId { get; set; }
+        public int GuestId { get; set; }
+
+        public virtual Concert Concert { get; set; }
     }
 }
